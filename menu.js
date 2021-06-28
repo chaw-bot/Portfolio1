81f16f21c1693a75539d04d03256329e48e21efe
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-06-28 18:45:21 +0200

    Fix linters

--- menu.js
+++ menu.js
@@ -4,26 +4,54 @@ const menuBtn = document.querySelector('.menu');
 const menuIcon = document.querySelector('nav .menu i');
 const envelope = document.querySelector('.fa-envelope');
 
-function openMenu() {
-  nav.classList.add('nav');
-  menuIcon.classList.replace('fa-bars', 'fa-times');
-  menuIcon.style.fontSize = '0.5em';
-  menuBtn.classList.add('align-end');
-  menuBtn.style.color = 'rgba(103, 121, 142, 1)';
-  navList.style.display = 'flex';
-  envelope.style.display = 'none';
-}
+// function openMenu() {
+//   nav.classList.add('nav');
+//   menuIcon.classList.replace('fa-bars', 'fa-times');
+//   menuIcon.style.fontSize = '0.5em';
+//   menuBtn.classList.add('align-end');
+//   menuBtn.style.color = 'rgba(103, 121, 142, 1)';
+//   navList.style.display = 'flex';
+//   envelope.style.display = 'none';
+// }
 
-function closeMenu() {
-  nav.classList.remove('nav');
-  menuIcon.classList.replace('fa-times', 'fa-bars');
-  navList.style.display = 'none';
-  envelope.style.display = 'inline-block';
-  menuIcon.style.fontSize = '1em';
-  menuBtn.style.color = '#fff';
-}
+// function closeMenu() {
+//   nav.classList.remove('nav');
+//   menuIcon.classList.replace('fa-times', 'fa-bars');
+//   navList.style.display = 'none';
+//   envelope.style.display = 'inline-block';
+//   menuIcon.style.fontSize = '1em';
+//   menuBtn.style.color = '#fff';
+// }
+
+// function menu() {
+//   if (navList.style.display === 'none') {
+//     openMenu();
+//   } else {
+//     closeMenu();
+//   }
+// }
 
 function menu() {
+
+  function openMenu() {
+    nav.classList.add('nav');
+    menuIcon.classList.replace('fa-bars', 'fa-times');
+    menuIcon.style.fontSize = '0.5em';
+    menuBtn.classList.add('align-end');
+    menuBtn.style.color = 'rgba(103, 121, 142, 1)';
+    navList.style.display = 'flex';
+    envelope.style.display = 'none';
+  }
+  
+  function closeMenu() {
+    nav.classList.remove('nav');
+    menuIcon.classList.replace('fa-times', 'fa-bars');
+    navList.style.display = 'none';
+    envelope.style.display = 'inline-block';
+    menuIcon.style.fontSize = '1em';
+    menuBtn.style.color = '#fff';
+  }
+
   if (navList.style.display === 'none') {
     openMenu();
   } else {
