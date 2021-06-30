4b33e81d40312bb4c98e39060a752012373b7d63
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-06-30 20:31:58 +0200

    Add modal and responsive button

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-//object
+//objects
 const projects = {
-    project1: {
-      title: 'Multi Post Stories',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  project1: {
+    title: 'Multi Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
@@ -11,17 +11,17 @@ const projects = {
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
       remaining essent`,
-      image: {
-        imageUrl: 'images/Portfolio.png',
-        imageAlt: 'Image with number 10 at the center',
-      },
-      technologies: ['html', 'Bootstrap', 'Ruby on rails'],
-      liveUrl: 'multipoststories.html',
-      soureUrl: 'multipoststoriesSource.html',
+    image: {
+      imageUrl: 'images/Portfolio.png',
+      imageAlt: 'Image with number 10 at the center',
     },
-    project2: {
-      title: 'Keeping track of hundreds  of components website',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    liveUrl: 'multipoststories.html',
+    soureUrl: 'multipoststoriesSource.html',
+  },
+  project2: {
+    title: 'Keeping track of hundreds  of components website',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
@@ -30,17 +30,17 @@ const projects = {
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
       remaining essent`,
-      image: {
-        imageUrl: 'images/Portfolio.png',
-        imageAlt: 'Image with number 10 at the center',
-      },
-      technologies: ['html', 'Bootstrap', 'Ruby on rails'],
-      liveUrl: 'multipoststories.html',
-      soureUrl: 'multipoststoriesSource.html',
+    image: {
+      imageUrl: 'images/Portfolio.png',
+      imageAlt: 'Image with number 10 at the center',
     },
-    project3: {
-      title: 'Project 3',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    liveUrl: 'multipoststories.html',
+    soureUrl: 'multipoststoriesSource.html',
+  },
+  project3: {
+    title: 'Project 3',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
@@ -49,17 +49,17 @@ const projects = {
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
       remaining essent`,
-      image: {
-        imageUrl: 'images/Portfolio.png',
-        imageAlt: 'Image with number 10 at the center',
-      },
-      technologies: ['html', 'css', 'javaScript'],
-      liveUrl: 'TonicLive.html',
-      soureUrl: 'Tonic.html',
+    image: {
+      imageUrl: 'images/Portfolio.png',
+      imageAlt: 'Image with number 10 at the center',
     },
-    project4: {
-      title: 'Project 4',
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'TonicLive.html',
+    soureUrl: 'Tonic.html',
+  },
+  project4: {
+    title: 'Project 4',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
@@ -68,61 +68,60 @@ const projects = {
       unknown printer took a galley of type and scrambled it to make a type specimen book. 
       It has survived not only five centuries, but also the leap into electronic typesetting, 
       remaining essent`,
-      image: {
-        imageUrl: 'images/Portfolio.png',
-        imageAlt: 'Image with number 10 at the center',
-      },
-      technologies: ['html', 'css', 'javaScript'],
-      liveUrl: 'mpStoriesLive.html',
-      soureUrl: 'mpStories.html',
+    image: {
+      imageUrl: 'images/Portfolio.png',
+      imageAlt: 'Image with number 10 at the center',
     },
-  };
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'mpStoriesLive.html',
+    soureUrl: 'mpStories.html',
+  },
+};
 
-  //Generate HTML
-  const popup = document.getElementsByClassName('popUpProject')[0];
+//Generate HTML
+const popup = document.getElementsByClassName('popUpProject')[0];
 
-  function generateModal(project) {
-    let htmlText = `<div class="modal" id="modal">
+function generateModal(project) {
+  let htmlText = `<button data-modal-target="#modal" class="orange-button">See Project</button>
+                    <div class="modal" id="modal">
                       <div class="modal-header">
-                       <div class="title">${project.title}</div>
-                       <button data-close-button class="close-button">&times;</button>
-                      </div>
-                      <div class="top-buttons">
-                        <ul class="buttons">`;
-    
-      for (let i = 0; i < project.technologies.length; i += 1 ){
+                        <div class="title">${project.title}</div>
+                          <button data-close-button class="close-button">&times;</button>
+                        </div>
+                        <div class="top-buttons">
+                          <ul class="buttons">`;
 
-      htmlText += `<li><button type="button" class="button"
-                >${project.technologies[i]}</button></li>`;
-    }
-      htmlText += `</ul>
-                    </div>              
-                    <div class="theBody">
-                    <div class="popUp-img">
-                      <img src="${project.image.imageUrl}" alt="${project.image.imageAlt}">
-                    </div>
-    
-                    <div class="body-button">
-                      <div class="modal-body">
-                      ${project.description}
-                      </div>
-    
-                      <div class="bt-buttons">
-                        <a href="${project.liveUrl}" class="orange-button">See Live  <i class="fa fa-external-link"></i></a>
-                        <a href="${project.sourceUrl}" class="orange-button">See Source  <i class="fa fa-github"></i></a>
-                      </div>
-                    </div>
-                  </div>  
-                </div>`;
+                            for (let i = 0; i < project.technologies.length; i += 1) {
 
-    return htmlText;
-  }
+                              htmlText += `<li><button type="button" class="button">${project.technologies[i]}</button></li>`;
+                            }
+                            htmlText += `
+                          </ul>
+                        </div>              
+                        <div class="theBody">
+                          <div class="popUp-img">
+                          <img src="${project.image.imageUrl}" alt="${project.image.imageAlt}">
+                        </div>
+    
+                        <div class="body-button">
+                          <div class="modal-body">${project.description}</div>
+    
+                          <div class="bt-buttons">
+                            <a href="${project.liveUrl}" class="orange-button">See Live  <i class="fa fa-external-link"></i></a>
+                            <a href="${project.sourceUrl}" class="orange-button">See Source  <i class="fa fa-github"></i></a>
+                          </div>
+                        </div>
+                      </div>  
+                    </div>`;
 
-  function insertHtml() {
-    popup.innerHTML += generateModal(projects.project1);
-  }
-  
-  popup.onload = insertHtml();
+  return htmlText;
+}
+
+function insertHtml() {
+  popup.innerHTML += generateModal(projects.project1);
+}
+
+popup.onload = insertHtml();
 
 //open close modals
 const openModalBtns = document.querySelectorAll('[data-modal-target]');
@@ -154,4 +153,4 @@ closeModalBtns.forEach(closeBtn => {
     closeModal(currentModal);
   });
 });
-  
+
