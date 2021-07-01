962c0b5e3c0ee8dfbdb3fa2d32203856871e06ed
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-01 12:14:49 +0200

    Add cards with JavaScript

--- popup.js
+++ popup.js
@@ -1,16 +1,104 @@
+//Array object
+// const projects = [
+//   {
+//      title: 'Multi Post Stories',
+//      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent`,
+//      image: {
+//        imageUrl: 'images/Placeholder.png',
+//        imageAlt: 'A woman doing a yoga pose',
+//      },
+//      technologies: ['css','html', 'bootstrap', 'Ruby'],
+//      liveUrl: 'multipoststories.html',
+//      soureUrl: 'multipoststoriesSource.html',
+//    },
+//   {
+//      title: 'Keeping track of hundreds  of components website',
+//      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent`,
+//      image: {
+//        imageUrl: 'images/Portfolio.png',
+//        imageAlt: 'Image with number 10 at the center',
+//      },
+//      technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+//      liveUrl: 'multipoststories.html',
+//      soureUrl: 'multipoststoriesSource.html',
+//    },
+//   {
+//      title: 'Project 3',
+//      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent`,
+//      image: {
+//        imageUrl: 'images/Portfolio.png',
+//        imageAlt: 'Image with number 10 at the center',
+//      },
+//      technologies: ['html', 'css', 'javaScript'],
+//      liveUrl: 'TonicLive.html',
+//      soureUrl: 'Tonic.html',
+//    },
+//   {
+//      title: 'Project 4',
+//      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+//      unknown printer took a galley of type and scrambled it to make a type specimen book.
+//      It has survived not only five centuries, but also the leap into electronic typesetting,
+//      remaining essent`,
+//      image: {
+//        imageUrl: 'images/Portfolio.png',
+//        imageAlt: 'Image with number 10 at the center',
+//      },
+//      technologies: ['html', 'css', 'javaScript'],
+//      liveUrl: 'mpStoriesLive.html',
+//      soureUrl: 'mpStories.html',
+//    },
+//   {
+//      title:'Professional Art',
+//      title2: 'Printing Data',
+//      description: `A daily selection of privately personalized reads; no accounts
+//                  or sign-ups required. has been the industry's standard`,
+//      image: {
+//        imageUrl: 'images/Portfolio.png',
+//        imageAlt: 'Image with number 10 at the center',
+//      },
+//      technologies: ['html', 'css', 'javaScript'],
+//      liveUrl: 'TonicLive.html',
+//      soureUrl: 'Tonic.html',
+//    }
+//  ];
+
 //object
 const projects = {
   project1: {
     title: 'Multi Post Stories',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
-    unknown printer took a galley of type and scrambled it to make a type specimen book. 
-    It has survived not only five centuries, but also the leap into electronic typesetting, 
-    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
-    unknown printer took a galley of type and scrambled it to make a type specimen book. 
-    It has survived not only five centuries, but also the leap into electronic typesetting, 
-    remaining essent`,
+    description: `A daily selection of privately personalized reads;
+    no accounts or sign-ups required. has been the industry's 
+    standard dummy text ever since the 1500s, 
+    when an unknown printer took a standard dummy text.`,
     image: {
       imageUrl: 'images/Placeholder.png',
       imageAlt: 'A woman doing a yoga pose',
@@ -76,6 +164,19 @@ const projects = {
     liveUrl: 'mpStoriesLive.html',
     soureUrl: 'mpStories.html',
   },
+  project5: {
+    title:'Professional Art',
+    title2: 'Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard`,
+    image: {
+      imageUrl: 'images/Portfolio.png',
+      imageAlt: 'Image with number 10 at the center',
+    },
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'TonicLive.html',
+    soureUrl: 'Tonic.html',
+  },
 };
 
 //Generate HTML
@@ -96,7 +197,7 @@ let htmlText = `<li class="card1">
                     <h3>${project.title}</h3>
 
                     <p>
-                      ${project.description}
+                      ${projects.project1.description}
                     </p>
 
                     <ul class="buttons">`;
@@ -112,35 +213,36 @@ let htmlText = `<li class="card1">
 
                     <button data-modal-target="#modal" class="orange-button">See Project</button>
                     </div>
-                </li>`;
+                </li>
+                </div>
+                    </li>`;
 return htmlText;
 }
 
 function generateModal(project) {
-  let htmlText = `<section class="popUpProject">
-                  <div class="modal" id="modal">
+  let htmlText = `<div class="modal" id="modal">
                     <div class="modal-header">
-                     <div class="title">${project.title}</div>
+                     <div class="title">${projects.project1.title}</div>
                      <button data-close-button class="close-button">&times;</button>
                     </div>
                     <div class="top-buttons">
                       <ul class="buttons">`;
   
-    for (let i = 0; i < project.technologies.length; i += 1 ){
+    for (let i = 0; i < projects.project2.technologies.length; i += 1 ){
 
     htmlText += `<li><button type="button" class="button"
-              >${project.technologies[i]}</button></li>`;
+              >${projects.project2.technologies[i]}</button></li>`;
   }
     htmlText += `</ul>
                   </div>              
                   <div class="theBody">
                   <div class="popUp-img">
-                    <img src="${project.image.imageUrl}" alt="${project.image.imageAlt}">
+                    <img src="${projects.project2.image.imageUrl}" alt="${projects.project2.image.imageAlt}">
                   </div>
   
                   <div class="body-button">
                     <div class="modal-body">
-                    ${project.description}
+                    ${projects.project2.description}
                     </div>
   
                     <div class="bt-buttons">
@@ -150,14 +252,163 @@ function generateModal(project) {
                   </div>
                 </div>  
               </div>
-              </section>`;
+              `;
 
   return htmlText;
 }
 
+function generateSecondCard(project) {
+  let htmlText = `<li>
+                    <ul class="card2container">
+                      <li class="card2 first">
+                        <h3>${projects.project5.title}<br/>${projects.project5.title2}</h3>
+
+                        <p>${projects.project5.description}</p>
+
+                        <ul class="buttons1">`;
+      for (let i = 0; i < projects.project2.technologies.length; i += 1 ){
+             htmlText += `<li><button type="button" class="button1">${projects.project2.technologies[i]}</button></li>`;
+      }
+             htmlText += `</ul>
+
+                        <button data-modal-target="#modal" type="button" class="orange-button orange-button3">
+                          See Project
+                        </button>
+                     </li> 
+                     
+
+                     <li class="card2 second">
+              <h3>
+                Professional Art <br />
+                Printing Data
+              </h3>
+
+              <p>
+                A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard
+              </p>
+
+              <ul class="buttons1">
+                <li><button type="button" class="button1">html</button></li>
+                <li>
+                  <button type="button" class="button1">bootstrap</button>
+                </li>
+                <li><button type="button" class="button1">Ruby</button></li>
+              </ul>
+
+              <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+                See Project
+              </button>
+            </li>
+
+            <li class="card2 third">
+            <h3>
+              Professional Art <br />
+              Printing Data
+            </h3>
+
+            <p>
+              A daily selection of privately personalized reads; no accounts
+              or sign-ups required. has been the industry's standard
+            </p>
+
+            <ul class="buttons1">
+              <li><button type="button" class="button1">html</button></li>
+              <li>
+                <button type="button" class="button1">bootstrap</button>
+              </li>
+              <li><button type="button" class="button1">Ruby</button></li>
+            </ul>
+
+            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+              See Project
+            </button>
+          </li>
+
+          <li class="card2 fourth">
+            <h3>
+              Professional Art <br />
+              Printing Data
+            </h3>
+
+            <p>
+              A daily selection of privately personalized reads; no accounts
+              or sign-ups required. has been the industry's standard
+            </p>
+
+            <ul class="buttons1">
+              <li><button type="button" class="button1">html</button></li>
+              <li>
+                <button type="button" class="button1">bootstrap</button>
+              </li>
+              <li><button type="button" class="button1">Ruby</button></li>
+            </ul>
+
+            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+              See Project
+            </button>
+          </li>
+
+          <li class="card2 fifth">
+            <h3>
+              Professional Art <br />
+              Printing Data
+            </h3>
+
+            <p>
+              A daily selection of privately personalized reads; no accounts
+              or sign-ups required. has been the industry's standard
+            </p>
+
+            <ul class="buttons1">
+              <li><button type="button" class="button1">html</button></li>
+              <li>
+                <button type="button" class="button1">bootstrap</button>
+              </li>
+              <li><button type="button" class="button1">Ruby</button></li>
+            </ul>
+
+            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+              See Project
+            </button>
+          </li>
+
+          <li class="card2 sixth">
+            <h3>
+              Professional Art <br />
+              Printing Data
+            </h3>
+
+            <p>
+              A daily selection of privately personalized reads; no accounts
+              or sign-ups required. has been the industry's standard
+            </p>
+
+            <ul class="buttons1">
+              <li><button type="button" class="button1">html</button></li>
+              <li>
+                <button type="button" class="button1">bootstrap</button>
+              </li>
+              <li><button type="button" class="button1">Ruby</button></li>
+            </ul>
+
+            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+              See Project
+            </button>
+          </li>
+        </ul>
+      </li>`;
+
+  return htmlText;
+}
+
+
+
+
 function insertHtml() {
   firstCardContainer.innerHTML += generateFirstCard(projects.project1);
   firstCardContainer.innerHTML += generateModal(projects.project1);
+  firstCardContainer.innerHTML += generateSecondCard(projects.project1);
 }
 
 firstCardContainer.onload = insertHtml();
