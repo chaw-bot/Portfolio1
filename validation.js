564f5d3ab01071faada881384be4d19266c2cb97
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-02 08:01:51 +0200

    Add form file

--- validation.js
+++ validation.js
@@ -1,16 +1,18 @@
-const cForm = document.getElementById('form');
+const Form = document.getElementById('form');
 const email = document.getElementById('email');
-const alertMsg = document.querySelector('.contact_form .alert_msg');
+const alertMsg = document.querySelector('.alert_msg');
 alertMsg.textContent = ' * Please change your email to lower case.';
-cForm.addEventListener('submit', (e) => {
-  if (email.value !== email.value.toLowerCase()) {
-    alertMsg.classList.add('active');
+//console.log(!email.value !== email.value.toLowerCase());
+
+email.addEventListener('invalid', (e) => {
+  e.preventDefault();
+  if (!email.value !== email.value.toLowerCase()) {
+    alertMsg.style.display='block';
     email.style.borderBottomLeftRadius = 0;
     email.style.borderBottomRightRadius = 0;
-    email.style.borderColor = '#aa161d';
+    email.style.borderColor = 'RED';
     email.style.borderStyle = 'solid';
     email.style.borderWidth = '1px';
-    e.preventDefault();
   } else {
     alertMsg.classList.remove('active');
   }
