3ecf7a72c067e266145e17e4a0951bd43c03d8dc
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-07-02 10:56:49 +0200

    Add function to check if input email is in the correct format and display alert message if incorrect

--- validation.js
+++ validation.js
@@ -1,19 +1,17 @@
-const Form = document.getElementById('form');
+const form = document.getElementById('form');
 const email = document.getElementById('email');
 const alertMsg = document.querySelector('.alert_msg');
 alertMsg.textContent = ' * Please change your email to lower case.';
 //console.log(!email.value !== email.value.toLowerCase());
 
-email.addEventListener('invalid', (e) => {
-  e.preventDefault();
-  if (!email.value !== email.value.toLowerCase()) {
-    alertMsg.style.display='block';
-    email.style.borderBottomLeftRadius = 0;
-    email.style.borderBottomRightRadius = 0;
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {    
+    alertMsg.style.display ='inline-block';
     email.style.borderColor = 'RED';
     email.style.borderStyle = 'solid';
     email.style.borderWidth = '1px';
+    e.preventDefault();
   } else {
-    alertMsg.classList.remove('active');
+    alertMsg.style.display ='none';
   }
 });
