50d1d4b2118977144f538be8b6f4b6fd54c41b15
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-02 11:07:06 +0200

    Add styling to Alert message

--- validation.js
+++ validation.js
@@ -1,17 +1,16 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const alertMsg = document.querySelector('.alert_msg');
-alertMsg.textContent = ' * Please change your email to lower case.';
-//console.log(!email.value !== email.value.toLowerCase());
+alertMsg.textContent = '! Email should be in lower case .';
 
 form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {    
     alertMsg.style.display ='inline-block';
-    email.style.borderColor = 'RED';
+    email.style.borderColor = 'red';
     email.style.borderStyle = 'solid';
     email.style.borderWidth = '1px';
     e.preventDefault();
   } else {
-    alertMsg.style.display ='none';
+    alertMsg.style.display ='flex';
   }
 });
