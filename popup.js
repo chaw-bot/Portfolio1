7b95aca71dccec18a33bd182d03947e2e2654a51
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-02 08:08:10 +0200

    Add modal to cards

--- popup.js
+++ popup.js
@@ -14,7 +14,7 @@ const projects = [
     soureUrl: 'multipoststoriesSource.html',
   },
   {
-    title: 'Keeping track of hundreds  of components website',
+    title: 'Keeping track',
     description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
      unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -31,45 +31,31 @@ const projects = [
     technologies: ['html', 'Bootstrap', 'Ruby on rails'],
     liveUrl: 'multipoststories.html',
     soureUrl: 'multipoststoriesSource.html',
+},
+{
+  title: 'Data Dashboard',
+  description: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  image: {
+    imageUrl: 'images/second.png',
+    imageAlt: 'background1',
   },
-  {
-    title: 'Project 3',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-     unknown printer took a galley of type and scrambled it to make a type specimen book.
-     It has survived not only five centuries, but also the leap into electronic typesetting,
-     remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-     unknown printer took a galley of type and scrambled it to make a type specimen book.
-     It has survived not only five centuries, but also the leap into electronic typesetting,
-     remaining essent`,
-    image: {
-      imageUrl: 'images/Portfolio.png',
-      imageAlt: 'Image with number 10 at the center',
-    },
-    technologies: ['html', 'css', 'javaScript'],
-    liveUrl: 'TonicLive.html',
-    soureUrl: 'Tonic.html',
+  technologies: ['html', 'bootstrap', 'Ruby'],
+  liveUrl: 'TonicLive.html',
+  soureUrl: 'Tonic.html',
+},
+{
+  title: 'Website Portfolio',
+  description: `A daily selection of privately personalized reads;
+   no accounts or sign-ups required. has been the industry's standard`,
+  image: {
+    imageUrl: 'images/third.png',
+    imageAlt: 'background1',
   },
-  {
-    title: 'Project 4',
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-     unknown printer took a galley of type and scrambled it to make a type specimen book.
-     It has survived not only five centuries, but also the leap into electronic typesetting,
-     remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-     unknown printer took a galley of type and scrambled it to make a type specimen book.
-     It has survived not only five centuries, but also the leap into electronic typesetting,
-     remaining essent`,
-    image: {
-      imageUrl: 'images/Portfolio.png',
-      imageAlt: 'Image with number 10 at the center',
-    },
-    technologies: ['html', 'css', 'javaScript'],
-    liveUrl: 'mpStoriesLive.html',
-    soureUrl: 'mpStories.html',
-  },
+  technologies: ['html', 'bootstrap', 'Ruby'],
+  liveUrl: 'TonicLive.html',
+  soureUrl: 'Tonic.html',
+},
   {
     title: 'Professional Art',
     title2: 'Printing Data',
@@ -83,6 +69,18 @@ const projects = [
     liveUrl: 'TonicLive.html',
     soureUrl: 'Tonic.html',
   },
+  {
+    title: 'Website Portfolio',
+    description: `A daily selection of privately personalized reads;
+     no accounts or sign-ups required. has been the industry's standard`,
+    image: {
+      imageUrl: 'images/third.png',
+      imageAlt: 'background1',
+    },
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    liveUrl: 'TonicLive.html',
+    soureUrl: 'Tonic.html',
+},
 ];
 
 // GenerateHTML
@@ -115,7 +113,7 @@ function generateFirstCard() {
   htmlText += `</ul>
                   <br />
 
-                    <button data-modal-target="#modal" class="orange-button">See Project</button>
+                    <button data-modal-target="#modals" class="orange-button">See Project</button>
                     </div>
                 </li>
                 </div>
@@ -124,6 +122,43 @@ function generateFirstCard() {
 }
 
 function generateModal() {
+  let htmlText = `<div class="modal" id="modals">
+                    <div class="modal-header">
+                     <div class="title">${projects[1].title}</div>
+                     <button data-close-button class="close-button">&times;</button>
+                    </div>
+                    <div class="top-buttons">
+                      <ul class="buttons">`;
+
+  for (let i = 0; i < projects[1].technologies.length; i += 1) {
+    htmlText += `<li><button type="button" class="button"
+              >${projects[1].technologies[i]}</button></li>`;
+  }
+  htmlText += `</ul>
+                  </div>              
+                  <div class="theBody">
+                  <div class="popUp-img">
+                    <img src="${projects[0].image.imageUrl}" alt="${projects[0].image.imageAlt}">
+                  </div>
+  
+                  <div class="body-button">
+                    <div class="modal-body">
+                    ${projects[0].description}
+                    </div>
+  
+                    <div class="bt-buttons">
+                      <a href="${projects[0].liveUrl}" class="orange-button">See Live       <i class="fa fa-external-link"></i></a>
+                      <a href="${projects[0].sourceUrl}" class="orange-button">See Source   <i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              `;
+
+  return htmlText;
+}
+
+function generateModal1() {
   let htmlText = `<div class="modal" id="modal">
                     <div class="modal-header">
                      <div class="title">${projects[0].title}</div>
@@ -146,6 +181,191 @@ function generateModal() {
                   <div class="body-button">
                     <div class="modal-body">
                     ${projects[1].description}
+                    </div>
+  
+                    <div class="bt-buttons">
+                      <a href="${projects[0].liveUrl}" class="orange-button">See Live       <i class="fa fa-external-link"></i></a>
+                      <a href="${projects[0].sourceUrl}" class="orange-button">See Source   <i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              `;
+
+  return htmlText;
+}
+
+function generateModal2() {
+  let htmlText = `<div class="modal" id="modal2">
+                    <div class="modal-header">
+                     <div class="title">${projects[1].title}</div>
+                     <button data-close-button class="close-button">&times;</button>
+                    </div>
+                    <div class="top-buttons">
+                      <ul class="buttons">`;
+
+  for (let i = 0; i < projects[1].technologies.length; i += 1) {
+    htmlText += `<li><button type="button" class="button"
+              >${projects[1].technologies[i]}</button></li>`;
+  }
+  htmlText += `</ul>
+                  </div>              
+                  <div class="theBody">
+                  <div class="popUp-img">
+                    <img src="${projects[2].image.imageUrl}" alt="${projects[2].image.imageAlt}">
+                  </div>
+  
+                  <div class="body-button">
+                    <div class="modal-body">
+                    ${projects[2].description}
+                    </div>
+  
+                    <div class="bt-buttons">
+                      <a href="${projects[0].liveUrl}" class="orange-button">See Live       <i class="fa fa-external-link"></i></a>
+                      <a href="${projects[0].sourceUrl}" class="orange-button">See Source   <i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              `;
+
+  return htmlText;
+}
+
+function generateModal3() {
+  let htmlText = `<div class="modal" id="modal3">
+                    <div class="modal-header">
+                     <div class="title">${projects[3].title}</div>
+                     <button data-close-button class="close-button">&times;</button>
+                    </div>
+                    <div class="top-buttons">
+                      <ul class="buttons">`;
+
+  for (let i = 0; i < projects[1].technologies.length; i += 1) {
+    htmlText += `<li><button type="button" class="button"
+              >${projects[1].technologies[i]}</button></li>`;
+  }
+  htmlText += `</ul>
+                  </div>              
+                  <div class="theBody">
+                  <div class="popUp-img">
+                    <img src="${projects[3].image.imageUrl}" alt="${projects[3].image.imageAlt}">
+                  </div>
+  
+                  <div class="body-button">
+                    <div class="modal-body">
+                    ${projects[3].description}
+                    </div>
+  
+                    <div class="bt-buttons">
+                      <a href="${projects[0].liveUrl}" class="orange-button">See Live       <i class="fa fa-external-link"></i></a>
+                      <a href="${projects[0].sourceUrl}" class="orange-button">See Source   <i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              `;
+
+  return htmlText;
+}
+
+function generateModal4() {
+  let htmlText = `<div class="modal" id="modal4">
+                    <div class="modal-header">
+                     <div class="title">${projects[4].title}</div>
+                     <button data-close-button class="close-button">&times;</button>
+                    </div>
+                    <div class="top-buttons">
+                      <ul class="buttons">`;
+
+  for (let i = 0; i < projects[1].technologies.length; i += 1) {
+    htmlText += `<li><button type="button" class="button"
+              >${projects[1].technologies[i]}</button></li>`;
+  }
+  htmlText += `</ul>
+                  </div>              
+                  <div class="theBody">
+                  <div class="popUp-img">
+                    <img src="${projects[4].image.imageUrl}" alt="${projects[4].image.imageAlt}">
+                  </div>
+  
+                  <div class="body-button">
+                    <div class="modal-body">
+                    ${projects[4].description}
+                    </div>
+  
+                    <div class="bt-buttons">
+                      <a href="${projects[0].liveUrl}" class="orange-button">See Live       <i class="fa fa-external-link"></i></a>
+                      <a href="${projects[0].sourceUrl}" class="orange-button">See Source   <i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              `;
+
+  return htmlText;
+}
+
+function generateModal5() {
+  let htmlText = `<div class="modal" id="modal5">
+                    <div class="modal-header">
+                     <div class="title">${projects[5].title}</div>
+                     <button data-close-button class="close-button">&times;</button>
+                    </div>
+                    <div class="top-buttons">
+                      <ul class="buttons">`;
+
+  for (let i = 0; i < projects[1].technologies.length; i += 1) {
+    htmlText += `<li><button type="button" class="button"
+              >${projects[1].technologies[i]}</button></li>`;
+  }
+  htmlText += `</ul>
+                  </div>              
+                  <div class="theBody">
+                  <div class="popUp-img">
+                    <img src="${projects[5].image.imageUrl}" alt="${projects[5].image.imageAlt}">
+                  </div>
+  
+                  <div class="body-button">
+                    <div class="modal-body">
+                    ${projects[5].description}
+                    </div>
+  
+                    <div class="bt-buttons">
+                      <a href="${projects[0].liveUrl}" class="orange-button">See Live       <i class="fa fa-external-link"></i></a>
+                      <a href="${projects[0].sourceUrl}" class="orange-button">See Source   <i class="fa fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>  
+              </div>
+              `;
+
+  return htmlText;
+}
+
+function generateModal6() {
+  let htmlText = `<div class="modal" id="modal1">
+                    <div class="modal-header">
+                     <div class="title">${projects[0].title}</div>
+                     <button data-close-button class="close-button">&times;</button>
+                    </div>
+                    <div class="top-buttons">
+                      <ul class="buttons">`;
+
+  for (let i = 0; i < projects[1].technologies.length; i += 1) {
+    htmlText += `<li><button type="button" class="button"
+              >${projects[1].technologies[i]}</button></li>`;
+  }
+  htmlText += `</ul>
+                  </div>              
+                  <div class="theBody">
+                  <div class="popUp-img">
+                    <img src="${projects[5].image.imageUrl}" alt="${projects[5].image.imageAlt}">
+                  </div>
+  
+                  <div class="body-button">
+                    <div class="modal-body">
+                    ${projects[5].description}
                     </div>
   
                     <div class="bt-buttons">
@@ -199,7 +419,7 @@ function generateSecondCard() {
                 <li><button type="button" class="button1">Ruby</button></li>
               </ul>
 
-              <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+              <button data-modal-target="#modal2" type="button" class="orange-button orange-button2">
                 See Project
               </button>
             </li>
@@ -223,7 +443,7 @@ function generateSecondCard() {
               <li><button type="button" class="button1">Ruby</button></li>
             </ul>
 
-            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+            <button data-modal-target="#modal3" type="button" class="orange-button orange-button2">
               See Project
             </button>
           </li>
@@ -247,7 +467,7 @@ function generateSecondCard() {
               <li><button type="button" class="button1">Ruby</button></li>
             </ul>
 
-            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+            <button data-modal-target="#modal4" type="button" class="orange-button orange-button2">
               See Project
             </button>
           </li>
@@ -271,7 +491,7 @@ function generateSecondCard() {
               <li><button type="button" class="button1">Ruby</button></li>
             </ul>
 
-            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+            <button data-modal-target="#modal5" type="button" class="orange-button orange-button2">
               See Project
             </button>
           </li>
@@ -295,7 +515,7 @@ function generateSecondCard() {
               <li><button type="button" class="button1">Ruby</button></li>
             </ul>
 
-            <button data-modal-target="#modal" type="button" class="orange-button orange-button2">
+            <button data-modal-target="#modal1" type="button" class="orange-button orange-button2">
               See Project
             </button>
           </li>
@@ -309,6 +529,12 @@ function insertHtml() {
   firstCardContainer.innerHTML += generateFirstCard(projects.project1);
   firstCardContainer.innerHTML += generateModal(projects.project1);
   firstCardContainer.innerHTML += generateSecondCard(projects.project1);
+  firstCardContainer.innerHTML += generateModal1(projects.project1);
+  firstCardContainer.innerHTML += generateModal2(projects.project1);
+  firstCardContainer.innerHTML += generateModal3(projects.project1);
+  firstCardContainer.innerHTML += generateModal4(projects.project1);
+  firstCardContainer.innerHTML += generateModal5(projects.project1);
+  firstCardContainer.innerHTML += generateModal6(projects.project1);
 }
 
 firstCardContainer.onload = insertHtml();
